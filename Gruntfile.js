8502e7b7322ad6ea96a0b3a4132e0f1f4fb8f8f9
ekompanets <amelie.kom@gmail.com>
2015-06-08 13:58:36 +0300

    Использование svg-спрайта

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,6 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-githooks');
-  grunt.loadNpmTasks('grunt-lintspaces');
+  require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -13,7 +8,7 @@ module.exports = function(grunt) {
     less: {
       style: {
         files: {
-          'css/style.css': 'less/style.less'
+          'build/css/style.css': 'source/less/style.less'
         }
       }
     },
@@ -58,6 +53,18 @@ module.exports = function(grunt) {
           ],
           dest: 'gosha',
         }]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**",
+            "js/**",
+            "*.html"
+            ],
+          dest: "build"
+        }]
       }
     },
 
@@ -65,7 +72,117 @@ module.exports = function(grunt) {
       gosha: [
         'gosha/img/README',
         'gosha/js/README'
-      ]
+      ],
+
+      build: ["build"]
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ["last 2 version","ie 10"]
+      },
+      style: {
+        src: "build/css/style.css"
+      }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          'build/css/style.css': ['build/css/style.css']
+        }
+      }
+    },
+
+    cssmin: {
+      style: {
+        options: {
+          keepSpecialComments: 0,
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css":["build/css/style.css"]
+        }
+      }
+    },
+
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["less/**/*.less"]
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif,svg}"]
+        }]
+      }
+    },
+
+    htmlmin: {                                     // Task 
+      options: {                                 // Target options 
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        caseSensitive:true,
+        keepClosingSlash: false
+      },
+      html: {
+        files: {                                   // Dictionary of files 
+          'build/index.min.html': 'build/index.html'
+        }
+      }
+    },
+
+    grunticon: {
+      icons: {
+        files: [{
+          expand: true,
+          cwd: "source/img/",
+          src: ["*.svg"],
+          dest: "build/css/"
+        }]
+        // Здесь опция enhanceSVG нам не нужна
+      }
+    },
+
+    svg_sprite: {
+      complex: { 
+        // Target basics 
+        expand: true,
+        cwd: 'source/img/',
+        src: ['*.svg'],
+        dest: 'build/img/',
+
+        // Target options 
+        options: {
+          shape: {
+            dimension: {         // Set maximum dimensions 
+              maxWidth: 32,
+              maxHeight: 32
+            },
+            spacing: {         // Add padding 
+              padding: 0
+            },
+            dest: 'intermediate-svg'    // Keep the intermediate files 
+        },
+        mode: {
+            view: {         // Activate the «view» mode 
+              bust: false,
+              render: {
+                  scss: false      // Activate Sass output (with default options) 
+              }
+            },
+            symbol: true      // Activate the «symbol» mode 
+          }
+        }
+      }
     }
   });
 
@@ -78,4 +195,16 @@ module.exports = function(grunt) {
   } else {
     grunt.registerTask('gosha', ['copy:gosha', 'clean:gosha']);
   }
+
+  grunt.registerTask("build",[
+    "clean:build",
+    "copy:build",
+    "svg_sprite",
+    "less",
+    "autoprefixer",
+    "cmq",
+    "cssmin",
+    "imagemin",
+    "htmlmin"
+  ]);
 };
